f9cf615394ed2ad4668d36075d9cef4b6b8f14c5
adrienfauquembergue <adrien_fauque@hotmail.fr>
2025-05-22 08:47:54 +0200

    fix code style

--- tests/Entity/PersistenceTest.php
+++ tests/Entity/PersistenceTest.php
@@ -2,19 +2,19 @@
 
 namespace App\Tests\Entity;
 
-use App\Kernel;
-use App\Entity\User;
 use App\Entity\Client;
-use App\Enum\UserRole;
 use App\Entity\Product;
-use Doctrine\ORM\Tools\SchemaTool;
+use App\Entity\User;
+use App\Enum\UserRole;
+use App\Kernel;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class PersistenceTest extends KernelTestCase
 {
     private static EntityManagerInterface $em;
-    
+
     protected static function getKernelClass(): string
     {
         return Kernel::class;
@@ -27,7 +27,7 @@ class PersistenceTest extends KernelTestCase
         /** @var EntityManagerInterface $em */
         self::$em = self::getContainer()->get('doctrine')->getManager();
 
-        $tool = new SchemaTool(self::$em);
+        $tool     = new SchemaTool(self::$em);
         $metadata = self::$em->getMetadataFactory()->getAllMetadata();
 
         // wipe & build
@@ -65,19 +65,19 @@ class PersistenceTest extends KernelTestCase
         self::$em->flush();
 
         $ids = [
-            'client'  => $client->getId(),
-            'product' => $product->getId(),
-            'user'    => $user->getId(),
-        ];
+                'client'  => $client->getId(),
+                'product' => $product->getId(),
+                'user'    => $user->getId(),
+               ];
         self::$em->clear();
 
-        $reloadedClient  = self::$em->find(Client::class,  $ids['client']);
+        $reloadedClient  = self::$em->find(Client::class, $ids['client']);
         $reloadedProduct = self::$em->find(Product::class, $ids['product']);
-        $reloadedUser    = self::$em->find(User::class,    $ids['user']);
+        $reloadedUser    = self::$em->find(User::class, $ids['user']);
 
-        self::assertSame('ACME',           $reloadedClient->getName());
-        self::assertSame('iPhone 15',      $reloadedProduct->getName());
-        self::assertSame('john@acme.com',  $reloadedUser->getEmail());
-        self::assertSame($reloadedClient,  $reloadedUser->getClient());
+        self::assertSame('ACME', $reloadedClient->getName());
+        self::assertSame('iPhone 15', $reloadedProduct->getName());
+        self::assertSame('john@acme.com', $reloadedUser->getEmail());
+        self::assertSame($reloadedClient, $reloadedUser->getClient());
     }
 }
